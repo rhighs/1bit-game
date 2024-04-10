ed54aae9b02895506c1ae763a781d6a851f4c91c
chrg <christian.ricci2000@gmail.com>
2024-04-10 21:08:29 +0200

    Add directory for assets and level data

---
+++ leveldata/random_tileset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="random_tileset" tilewidth="33" tileheight="32" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="33" height="32" source="../assets/left.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../assets/middle.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../assets/right.png"/>
+ </tile>
+</tileset>

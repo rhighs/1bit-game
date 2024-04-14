f490b21cc6376580c2e63d5e309c6c80f8521f92
chrg <christian.ricci2000@gmail.com>
2024-04-14 22:07:14 +0200

    Add python print utils

--- util.lua
+++ util.lua
@@ -6,6 +6,31 @@ local util = {}
 function util.Color(r, g, b, a) return ffi.new("Color", r, g, b, a) end
 function util.Rec(x, y, w, h) return ffi.new("Rectangle", x, y, w, h) end
 function util.sign(a) return a >= 0 and 1 or -1 end
-function util.print(text) rl.DrawText(text, 10, 10, 20, color.COLOR_PRIMARY) end
+function util.print(text, y) rl.DrawText(text, 0, y * 24, 24, color.COLOR_PRIMARY) end
+
+function util.tabfmt(t)
+    local res = "{"
+    for k, v in pairs(t) do
+        -- s = type(v) == "table" and tabfmt(t) or tostring(v)
+        res = res .. "[" .. tostring(k) .. "] = "
+        s = type(v) == "table" and util.tabfmt(v) or tostring(v)
+        res = res .. s .. ", "
+    end
+    res = res:sub(1, -3) .. "}"
+    return res
+end
+
+function util.pyfmt(...)
+    local res = ""
+    for k, v in ipairs{...} do
+        s = type(v) == "table" and util.tabfmt(v) or tostring(v)
+        res = res .. s .. " "
+    end
+    return res
+end
+
+function util.pyprint(...)
+    print(util.pyfmt(...))
+end
 
 return util

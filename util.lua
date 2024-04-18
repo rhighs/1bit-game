e3e279e9fcc28f5849b6e2ef4f58290705625d07
chrg <christian.ricci2000@gmail.com>
2024-04-18 22:27:56 +0200

    util: update pyprint, add sign

--- util.lua
+++ util.lua
@@ -8,29 +8,35 @@ function util.Rec(x, y, w, h) return ffi.new("Rectangle", x, y, w, h) end
 function util.sign(a) return a >= 0 and 1 or -1 end
 function util.print(text, y) rl.DrawText(text, 0, y * 24, 24, color.COLOR_PRIMARY) end
 
-function util.tabfmt(t)
-    local res = "{"
+function util.format_table(t)
+    local s = "{"
     for k, v in pairs(t) do
-        -- s = type(v) == "table" and tabfmt(t) or tostring(v)
-        res = res .. "[" .. tostring(k) .. "] = "
-        s = type(v) == "table" and util.tabfmt(v) or tostring(v)
-        res = res .. s .. ", "
+        s = s .. "[" .. util.pystr(k) .. "] = " .. util.pystr(v) .. ", "
     end
-    res = res:sub(1, -3) .. "}"
-    return res
+    return s == "{" and "{}" or s:sub(1, -3) .. "}"
 end
 
-function util.pyfmt(...)
-    local res = ""
-    for k, v in ipairs{...} do
-        s = type(v) == "table" and util.tabfmt(v) or tostring(v)
-        res = res .. s .. " "
+function util.pystr(...)
+    local args = { n = select('#', ...); ... }
+    local s = ""
+    for i = 1, args.n do
+        if type(args[i]) == "table" then
+            s = s .. util.format_table(args[i]) .. " "
+        else
+            s = s .. tostring(args[i]) .. " "
+        end
     end
-    return res
+    return s:sub(1, -2)
 end
 
 function util.pyprint(...)
-    print(util.pyfmt(...))
+    print(util.pystr(...))
+end
+
+function util.sign(x)
+    return x < 0 and -1
+        or x > 0 and 1
+        or 0
 end
 
 return util

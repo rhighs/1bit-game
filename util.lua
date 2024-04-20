0a1d90a04bf9216df6c8a65a3837e07b2ace0fa3
roberto montalti <roberto.montalti00@gmail.com>
2024-04-20 12:16:46 +0200

    util: improved printing function by adding indentation options

--- util.lua
+++ util.lua
@@ -8,25 +8,51 @@ function util.Rec(x, y, w, h) return ffi.new("Rectangle", x, y, w, h) end
 function util.sign(a) return a >= 0 and 1 or -1 end
 function util.print(text, y) rl.DrawText(text, 0, y * 24, 24, color.COLOR_PRIMARY) end
 
-function util.format_table(t)
-    local s = "{"
-    for k, v in pairs(t) do
-        s = s .. "[" .. util.pystr(k) .. "] = " .. util.pystr(v) .. ", "
-    end
-    return s == "{" and "{}" or s:sub(1, -3) .. "}"
+util.DEFAULT_FORMAT_INDENT = 4
+
+function render_indent(indent)
+    return indent and string.rep(" ", indent) or "" 
 end
 
-function util.pystr(...)
+function _format_table(t, indent)
+    local s = ""
+    local keys = {}
+    for k in pairs(t) do
+        table.insert(keys, k)
+    end
+
+    if #keys == 0 then
+        return "{}"
+    end
+
+    for i, k in ipairs(keys) do
+        local v = t[k]
+        local body = render_indent(indent) .. "[" .. _pystr({indent=4}, k) .. "] = " .. _pystr({indent = indent+util.DEFAULT_FORMAT_INDENT}, v)
+        s = s .. body .. (i < (#keys) and "," or "") .. "\n"
+    end
+
+    return "{\n" .. s .. render_indent(indent - util.DEFAULT_FORMAT_INDENT) .. "}"
+end
+
+function _pystr(opts, ...)
     local args = { n = select('#', ...); ... }
     local s = ""
     for i = 1, args.n do
         if type(args[i]) == "table" then
-            s = s .. util.format_table(args[i]) .. " "
+            s = s .. _format_table(args[i], opts.indent) .. " "
         else
             s = s .. tostring(args[i]) .. " "
         end
     end
     return s:sub(1, -2)
+end
+
+function util.format_table(t)
+    return _format_table(t, util.DEFAULT_FORMAT_INDENT)
+end
+
+function util.pystr(...)
+    return _pystr({ indent = util.DEFAULT_FORMAT_INDENT }, ...)
 end
 
 function util.pyprint(...)

e7a24d678d3c8b450041aa0caa0e22d768ac013f
chrg <christian.ricci2000@gmail.com>
2024-04-20 19:15:48 +0200

    Refine ghost movement

--- ghost.lua
+++ ghost.lua
@@ -3,34 +3,32 @@ local vec = require "vec"
 
 local ghost = {}
 
+local entity = {}
+
+function entity:update(dt)
+    local old_pos = self.pos
+    self.pos = vec.v2(
+        self.start_pos.x + math.cos(self.n/60) * 100,
+        self.start_pos.y + math.sin(self.n/15) * 20
+    )
+    self.n = self.n + 1
+    self.dir = (self.pos.x - old_pos.x < 0) and -1 or 1
+end
+
+function entity:draw()
+    rl.DrawTextureV(self.dir == -1 and self.left or self.right, self.pos, rl.WHITE)
+end
+
 function ghost.new(spawn_pos)
-    return {
-        left = rl.LoadTexture("assets/ghost.png"),
-        right = rl.LoadTexture("assets/ghost-right.png"),
+    entity.__index = entity
+    return setmetatable({
+        start_pos = spawn_pos,
         pos = spawn_pos,
-        dir = -1,
         n = 0,
-        frame_counter = 0,
-
-        update = function (self, dt)
-            self.pos = vec.v2(
-                self.pos.x + self.dir * 4,
-                self.pos.y + math.sin(self.n/10) * 4
-            )
-            self.n = self.n + 1
-            if self.n % 100 == 0 then
-                self.dir = -self.dir
-            end
-        end,
-
-        draw = function (self)
-            if self.dir == -1 then
-                rl.DrawTextureV(self.left, self.pos, rl.WHITE)
-            else
-                rl.DrawTextureV(self.right, self.pos, rl.WHITE)
-            end
-        end,
-    }
+        dir = -1,
+        left = rl.LoadTexture("assets/ghost.png"),
+        right = rl.LoadTexture("assets/ghost-right.png")
+    }, entity)
 end
 
 return ghost

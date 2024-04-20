07edcaa486791bf82cf8b09010d93bf9c01db848
chrg <christian.ricci2000@gmail.com>
2024-04-20 17:34:40 +0200

    Add game over scene

---
+++ game-over-scene.lua
@@ -0,0 +1,26 @@
+local game_over = {}
+
+local scene = {}
+
+function scene:init()
+end
+
+function scene:update()
+    self.n = self.n - 1
+end
+
+function scene:draw()
+    local width = rl.MeasureText("game over.", 32)
+    rl.DrawText("game over.", 800/2 - width/2, 450/2 - 32/2, 32, rl.WHITE)
+end
+
+function scene:should_change()
+    return self.n == 0 and { name = "start" } or nil
+end
+
+function game_over.new(duration)
+    scene.__index = scene
+    return setmetatable({ n = duration }, scene)
+end
+
+return game_over
--- main.lua
+++ main.lua
@@ -7,6 +7,7 @@ local level_loader = require "level_loader"
 local start_screen_controller = require "start-screen-controller"
 local level_scene = require "level-scene"
 local physics = require("physics")
+local game_over = require "game-over-scene"
 
 local VP_WIDTH, VP_HEIGHT = 800, 450
 local VP = vec.v2(VP_WIDTH, VP_HEIGHT)
@@ -18,7 +19,8 @@ rl.SetTargetFPS(60)
 local scenes = {
     list = {
         ["start"] = start_screen_controller.new(),
-        ["level"] = level_scene.new()
+        ["level"] = level_scene.new(),
+        ["gameover"] = game_over.new()
     },
     cur = "start",
 

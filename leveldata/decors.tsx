16c7c172c9473436e187b9494427de8d71729479
roberto montalti <roberto.montalti00@gmail.com>
2024-05-29 00:19:24 +0200

    remove unnecessary ppath variants

--- leveldata/decors.tsx
+++ leveldata/decors.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="decors" tilewidth="96" tileheight="96" tilecount="26" columns="0">
+<tileset version="1.8" tiledversion="1.8.2" name="decors" tilewidth="96" tileheight="96" tilecount="11" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
   <image width="32" height="32" source="../assets/column_bottom.png"/>
@@ -22,61 +22,16 @@
  <tile id="11">
   <image width="32" height="32" source="../assets/ppath-d-asc.png"/>
  </tile>
- <tile id="12">
-  <image width="32" height="32" source="../assets/ppath-d-desc.png"/>
- </tile>
- <tile id="13">
-  <image width="32" height="32" source="../assets/ppath-h-jbl.png"/>
- </tile>
- <tile id="14">
-  <image width="32" height="32" source="../assets/ppath-h-jbr.png"/>
- </tile>
- <tile id="15">
-  <image width="32" height="32" source="../assets/ppath-h-jtl.png"/>
- </tile>
- <tile id="16">
-  <image width="32" height="32" source="../assets/ppath-h-jtr.png"/>
- </tile>
- <tile id="17">
-  <image width="32" height="32" source="../assets/ppath-h.png"/>
- </tile>
  <tile id="18">
   <image width="32" height="32" source="../assets/ppath-v-jbl.png"/>
  </tile>
- <tile id="19">
-  <image width="32" height="32" source="../assets/ppath-v-jbr.png"/>
- </tile>
- <tile id="20">
-  <image width="32" height="32" source="../assets/ppath-v-jtl.png"/>
- </tile>
- <tile id="21">
-  <image width="32" height="32" source="../assets/ppath-v-jtr.png"/>
- </tile>
  <tile id="22">
   <image width="32" height="32" source="../assets/ppath-v.png"/>
- </tile>
- <tile id="31">
-  <image width="32" height="32" source="../assets/ppath-d-ebl.png"/>
- </tile>
- <tile id="32">
-  <image width="32" height="32" source="../assets/ppath-d-ebr.png"/>
- </tile>
- <tile id="33">
-  <image width="32" height="32" source="../assets/ppath-d-etl.png"/>
  </tile>
  <tile id="34">
   <image width="32" height="32" source="../assets/ppath-d-etr.png"/>
  </tile>
  <tile id="35">
   <image width="32" height="32" source="../assets/ppath-h-el.png"/>
- </tile>
- <tile id="36">
-  <image width="32" height="32" source="../assets/ppath-v-eb.png"/>
- </tile>
- <tile id="37">
-  <image width="32" height="32" source="../assets/ppath-v-er.png"/>
- </tile>
- <tile id="38">
-  <image width="32" height="32" source="../assets/ppath-v-et.png"/>
  </tile>
 </tileset>

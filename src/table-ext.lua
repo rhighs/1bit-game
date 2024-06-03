9dd51012a011872a4e3280a54ba1250716e52943
roberto montalti <roberto.montalti00@gmail.com>
2024-06-03 05:51:33 +0200

    added some vec and table utilities

--- src/table-ext.lua
+++ src/table-ext.lua
@@ -68,6 +68,18 @@ function table.sorted(t, cmp)
     return result
 end
 
+function table.max(t, callable)
+    if callable == nil then callable = function (v) return v end end
+    if #t == 0 then return nil, nil end
+    local key, value = 1, t[1]
+    for i = 2, #t do
+        if callable(value) < callable(t[i]) then
+            key, value = i, t[i]
+        end
+    end
+    return key, value
+end
+
 function table.foldl(t, init, fn)
     local r = init
     for _, v in ipairs(t) do

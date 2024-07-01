9598ec5828510dc90c98b4d11494ce1cd5afe45a
roberto montalti <roberto.montalti00@gmail.com>
2024-07-01 23:21:05 +0200

    fixing broken level stuff due to merge conflicts

--- leveldata/decor.tsx
+++ leveldata/decor.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="decor" tilewidth="32" tileheight="32" tilecount="32" columns="8">
- <image source="../assets/decor.png" width="256" height="128"/>
+<tileset version="1.10" tiledversion="1.10.2" name="decor" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="../assets/decor.png" width="256" height="160"/>
+ <tile id="1">
+  <properties>
+   <property name="ppath_point" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="ppath_point" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="ppath_point" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="ppath_point" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="ppath_point" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="ppath_point" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
